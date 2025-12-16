19cedd7a6c5b4c3f5b3e05a3c37c5cfca4fe5ef0
vuk ivanovic <vuk.ivanovic@zurich.com>
2025-12-16 11:09:14 +0100

    feat: create vite + folder structuring

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { UsersPage } from './pages/UsersPage'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<UsersPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
