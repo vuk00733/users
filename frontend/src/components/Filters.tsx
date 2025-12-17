6b6e12f8c70a60a01da6167928fe39444404b5eb
vuk ivanovic <vuk.ivanovic@zurich.com>
2025-12-17 10:35:01 +0100

    feat: memoizing filter options

--- frontend/src/components/Filters.tsx
+++ frontend/src/components/Filters.tsx
@@ -1,5 +1,5 @@
 import { MenuItem, Stack, TextField } from "@mui/material";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { fetchCountries, fetchRoles } from "../api/users";
 import type { Country, Role, UsersQuery } from "../types/users";
 
@@ -61,26 +61,30 @@ export const Filters = ({ query, onChange }: Props) => {
 
 	useEffect(() => {
 		fetchCountries().then((data) =>
-			setCountries(
-				[...data].sort((a, b) => a.name.localeCompare(b.name)),
-			),
+			setCountries([...data].sort((a, b) => a.name.localeCompare(b.name))),
 		);
 		fetchRoles().then((data) =>
-			setRoles(
-				[...data].sort((a, b) => a.name.localeCompare(b.name)),
-			),
+			setRoles([...data].sort((a, b) => a.name.localeCompare(b.name))),
 		);
 	}, []);
 
-	const countryOptions = countries.map((country) => ({
-		value: String(country.id),
-		label: country.name,
-	}));
+	const countryOptions = useMemo(
+		() =>
+			countries.map((country) => ({
+				value: String(country.id),
+				label: country.name,
+			})),
+		[countries],
+	);
 
-	const roleOptions = roles.map((role) => ({
-		value: role.name,
-		label: role.name,
-	}));
+	const roleOptions = useMemo(
+		() =>
+			roles.map((role) => ({
+				value: role.name,
+				label: role.name,
+			})),
+		[roles],
+	);
 
 	return (
 		<Stack
